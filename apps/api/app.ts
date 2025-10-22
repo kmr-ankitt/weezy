3e8b1801ceb29fa5420b508c141c3795df60eb68
ankit <sahuankit.murhu@gmail.com>
2025-10-22 22:24:28 +0530

    chore: abstracting prisma out of api

--- apps/api/app.ts
+++ apps/api/app.ts
@@ -1,10 +1,3 @@
 import * as dotenv from 'dotenv'
-import { prisma } from './prisma/client'
 
 dotenv.config()
-
-prisma.$connect().then(() => {
-  console.log('Connected to the database successfully.')
-}).catch((error) => {
-  console.error('Error connecting to the database:', error)
-})    
---
+++ apps/prisma/app.ts
@@ -0,0 +1,10 @@
+import * as dotenv from 'dotenv'
+import { prisma } from './client'
+
+dotenv.config()
+
+prisma.$connect().then(() => {
+  console.log('Connected to the database successfully.')
+}).catch((error) => {
+  console.error('Error connecting to the database:', error)
+})    
