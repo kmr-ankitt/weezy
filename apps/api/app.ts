8f36ebcb8a730700496edc1e15bd723ebce597e0
ankit <sahuankit.murhu@gmail.com>
2025-10-22 21:45:32 +0530

    feat: prisma setup
    feat: prisma setup

--- apps/api/app.ts
+++ apps/api/app.ts
@@ -1,1 +1,10 @@
-console.log("Hellow");
+import * as dotenv from 'dotenv'
+import { prisma } from './prisma/client'
+
+dotenv.config()
+
+prisma.$connect().then(() => {
+  console.log('Connected to the database successfully.')
+}).catch((error) => {
+  console.error('Error connecting to the database:', error)
+})    
