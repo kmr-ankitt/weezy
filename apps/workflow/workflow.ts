d17683642cee751d46e89a6631e36c5a2512ef00
ankit <sahuankit.murhu@gmail.com>
2025-10-24 20:03:58 +0530

    feat: worflow setters and getters

--- apps/workflow/workflow.ts
+++ apps/workflow/workflow.ts
@@ -26,4 +26,32 @@ export class Workflow {
     this.connections = param.connections;
     this.settings = param.settings;
   }
+
+  setName(name: string) {
+    this.name = name;
+  }
+
+  setNodes(node: INode) {
+    this.nodes.push(node);
+  }
+
+  setConnection(connection: IConnection) {
+    this.connections.push(connection);
+  }
+
+  setSettings(settings: WorkflowSettings) {
+    this.settings = settings;
+  }
+
+  getNodes(): INode[] {
+    return this.nodes;
+  }
+
+  getNode(id: string): INode | undefined {
+    return this.nodes.find((node: INode) => node.id === id);
+  }
+
+  getConnections(): IConnection[] {
+    return this.connections;
+  }
 }
